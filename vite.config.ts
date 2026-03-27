07a39f1081b88d90d74f10f42e38c4f0181519fb
irinabudiakina <ibudiakina@gmail.com>
2026-03-27 17:44:57 +0300

    [feature] added deploy.sh

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/sound-scape/',
   plugins: [
     vue(),
     vueDevTools(),
